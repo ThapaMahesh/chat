cfcc6a242c626ae2de97bf76f6dd59c20c6f0a26
Mahesh Thapa <maheshb@stud.ntnu.no>
2017-12-22 00:25:35 +0100

    save and update

--- waypoints/src/app/app.component.ts
+++ waypoints/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MouseEvent } from '@agm/core';
 
 import {GoogleMapsAPIWrapper} from '@agm/core';
+import { saveAs } from 'file-saver/FileSaver';
 
 declare var google: any;
 @Component({
@@ -36,23 +37,11 @@ export class AppComponent  {
       label: "Marker " + (this.markers.length+1),
       draggable: true
     });
-
-    // if(this.markers.length == 2){
-    // 	this.showDirection();
-    // }
   }
-  
-  // public showDirection(): void{
-  // 	this.dir = {
-  //       org: { lat: this.markers[0].lat, lng: this.markers[0].lng },
-  //       dest: { lat: this.markers[1].lat, lng: this.markers[1].lng }
-  //     }
-  // }
 
   markerDragEnd(i, $event: MouseEvent) {
     this.markers[i].lat = $event.coords.lat;
     this.markers[i].lng = $event.coords.lng;
-    // this.showDirection();
   }
   
   markers: marker[] = []
@@ -62,10 +51,16 @@ export class AppComponent  {
 
   public saveWay(): void{
 
+  	this.waypoints = [];
   	for(var i=0; i<this.markers.length; i++){
   		this.waypoints.push({lat: this.markers[i].lat, lng: this.markers[i].lng});
   	}
   	console.log(this.waypoints[1].lat);
+  }
+
+  public removeWaypoint(index): void{
+  	this.waypoints.splice(index, 1);
+  	this.markers.splice(index, 1);
   }
 
   public addPoints($event, i): void{
@@ -83,16 +78,53 @@ export class AppComponent  {
 	}
   }
 
+  public removeTourWaypoints(tourindex, waypointIndex): void{
+  	this.tours[tourindex].waypoints.splice(waypointIndex, 1);
+  }
+
   public saveTour(): void{
-  	var thisTour = [];
+  	var thisTour = {start: {}, waypoints: [], destination: {} };
   	for(var i=0; i<this.tourpoints.length; i++){
-  		thisTour.push({
-  			lat: this.tourpoints[i].lat,
-  			lng: this.tourpoints[i].lng
-  		});
+  		if(i==0){
+  			thisTour.start = {
+	  			lat: this.tourpoints[i].lat,
+	  			lng: this.tourpoints[i].lng
+	  		}
+  		}
+
+  		else if(i==this.tourpoints.length-1){
+  			thisTour.destination = {
+	  			lat: this.tourpoints[i].lat,
+	  			lng: this.tourpoints[i].lng
+	  		}
+  		}
+  		else {
+	  		thisTour.waypoints.push({
+	  			lat: this.tourpoints[i].lat,
+	  			lng: this.tourpoints[i].lng
+	  		});
+	  	}
   	}
   	this.tours.push(thisTour);
   	console.log(this.tours);
+  }
+
+  public removeTour(index): void{
+  	this.tours.splice(index, 1);
+  }
+
+  private dispatchWaypoint() {
+    var waypointData = JSON.stringify(this.waypoints);
+
+    const blob = new Blob([waypointData], { type: 'text/json' });
+    saveAs(blob, 'waypoints.json');
+  }
+
+  private dispatchTour() {
+    var tourData = JSON.stringify(this.tours);
+    
+    const blob = new Blob([tourData], { type: 'text/json' });
+    saveAs(blob, 'tours.json');
   }
 }
 
